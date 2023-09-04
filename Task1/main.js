b4717e385a0149b037d544f2a806bd9501fc745c
Anton Meshchaninov <tonymsnv@gmail.com>
2023-09-04 18:06:24 +0300

    Updated Task1

--- Task1/main.js
+++ Task1/main.js
@@ -5,10 +5,58 @@ const rl = readline.createInterface({
   output: process.stdout,
 })
 
+// 3 13 12 4 14 6
+
 const lines = []
+// const lines = ['6', '3 13 12 4 14 6']
+// const lines = ['4', '-1 12 7 3']
 
 rl.on('line', (line) => {
   lines.push(line)
 }).on('close', () => {
-  console.log('Result: ', lines)
+  const count = lines[0]
+  const nails = lines[1]
+    .split(' ')
+    .map(Number)
+    .sort((a, b) => a - b)
+
+  let result = 0
+  let prevLink = false
+
+  result += nails[1] - nails[0]
+  prevLink = true
+
+  if (count > 2) {
+    for (let i = 1; i < count - 1; i++) {
+      const leftLength = nails[i] - nails[i - 1]
+      const rightLength = nails[i + 1] - nails[i]
+      const choice = leftLength - rightLength > 0 ? 'right' : 'left'
+
+      if (choice === 'left') {
+        if (prevLink || i === count - 2) {
+          prevLink = false
+        } else {
+          result += leftLength
+          prevLink = false
+        }
+
+        prevLink = false
+      } else {
+        if (prevLink) {
+          prevLink = false
+        } else {
+          result += rightLength
+
+          prevLink = true
+        }
+      }
+    }
+  }
+
+  if (!prevLink && count > 2) {
+    result += nails.at(-1) - nails.at(-2)
+  }
+
+  console.log(result)
 })
+// .close()
